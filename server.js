50205c02e2c4b96a76d0569cb5dde8752d1f1501
Richtofen4 <dawid.ciurkot@interia.pl>
2025-11-14 14:26:48 +0100

    poprawki

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+/* LOKALNIE
 //CORS
 const corsOptions = {
     origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'],
@@ -23,6 +23,24 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'x-access-token', 'Authorization', 'Accept', 'Origin'],
 };
 app.use(cors(corsOptions));
+*/
+
+//GLOBALNIE
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'http://localhost:5174',
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  allowedHeaders: ['Content-Type', 'x-access-token', 'Authorization', 'Accept', 'Origin'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 
 //Swagger
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -36,6 +54,8 @@ db.sequelize.sync().then(() => {
 
 // Socker.io
 const server = http.createServer(app);
+//LOKALNIE
+/*
 const io = new Server(server, {
   path: '/socket.io',
   cors: {
@@ -44,7 +64,16 @@ const io = new Server(server, {
     credentials: true,
   },
 });
-
+*/
+//GLOBALNIE
+const io = new Server(server, {
+  path: '/socket.io',
+  cors: {
+    origin: allowedOrigins,
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
 app.locals.io = io;
 
 io.on("connection", (socket) => {
