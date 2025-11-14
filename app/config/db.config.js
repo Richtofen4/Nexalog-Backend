1954cdc2d6367d4467d6b2e3a302b304b886c0f3
Richtofen4 <dawid.ciurkot@interia.pl>
2025-11-14 12:55:24 +0100

    Backend4

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,12 @@
 // ustawienia dockera
+/*
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
     PASSWORD: "postgres",
     DB: "postgres",
     port: 5432,
-    dialect: "postgres",
+    dialect: "postgres",asd
     pool: {
         max: 100,
         min: 0,
@@ -13,4 +14,20 @@ module.exports = {
         idle: 20000
     },
     logging: false
+};
+*/
+
+module.exports = {
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  PORT: Number(process.env.DB_PORT) || 5432,
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
